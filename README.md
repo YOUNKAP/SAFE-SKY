2136c7691ff32461d8e9a05e7d799f345e7bdb9a
YOUNKAP <nina.younkap@ccaa.pro>
2024-02-07 22:15:36 -0500

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+## SAFE SKY
+
+SAFE SKY App  is a Web-GIS application which delineates  airspace where drone flights could cause hazard to aircraft operation
+
---
+++ projects/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+    technology = models.CharField(max_length=500)
+    image = models.FileField(upload_to="project_images/", blank=True)
